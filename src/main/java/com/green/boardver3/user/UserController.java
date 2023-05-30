3135868c4a691d91538faa196fc1bfd0744845eb
JeongHoBBang <dlawjdghbn@naver.com>
2023-05-30 12:16:34 +0900

    userget

--- src/main/java/com/green/boardver3/user/UserController.java
+++ src/main/java/com/green/boardver3/user/UserController.java
@@ -4,6 +4,7 @@ import com.green.boardver3.user.model.UserInsDto;
 import com.green.boardver3.user.model.UserReDto;
 import com.green.boardver3.user.model.UserSelDto;
 import com.green.boardver3.user.model.UserSelVo;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,12 @@ public class UserController {
         this.service = service;
     }
     @PostMapping
+    @Operation(summary = "회원가입", description = ""+
+            "uid:[20] 회원 아이디,<br>"+
+            "upw:[100] 회원 비밀번호,<br>"+
+            "nm: [30] 회원명,<br>"+
+            "gender: [1] 성별(M: 남성, F: 여성),<br>"+
+            "addr: [100] 대구시 달서구")
     public int postuser(@RequestBody UserInsDto dto){
         return service.insUser(dto);
 
