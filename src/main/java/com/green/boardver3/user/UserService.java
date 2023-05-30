f5a33475d135495437104fbffb1969bef14990a7
minzion0 <aert12435@gmail.com>
2023-05-30 12:47:29 +0900

    암호화

--- src/main/java/com/green/boardver3/user/UserService.java
+++ src/main/java/com/green/boardver3/user/UserService.java
@@ -4,22 +4,29 @@ import com.green.boardver3.user.model.UserInsDto;
 import com.green.boardver3.user.model.UserReDto;
 import com.green.boardver3.user.model.UserSelDto;
 import com.green.boardver3.user.model.UserSelVo;
+import com.green.boardver3.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserMapper mapper;
+    private final CommonUtils commonUtils;
     @Autowired
-    public UserService(UserMapper mapper) {
+    public UserService(UserMapper mapper,CommonUtils commonUtils) {
+        this.commonUtils = commonUtils;
         this.mapper = mapper;
     }
 
     public int insUser(UserInsDto dto){
+        String hpw = commonUtils.encodeSha256(dto.getUpw());
+        dto.setUpw(hpw);
         return mapper.insUser(dto);
     }
 
     public UserSelVo selUser(UserSelDto dto){ //앞 보여주는거 안 입력받는거!
+        String hpw = commonUtils.encodeSha256(dto.getUpw());
+        dto.setUpw(hpw);
         return mapper.selUser(dto);
     }
     public int repUser(UserReDto dto){
