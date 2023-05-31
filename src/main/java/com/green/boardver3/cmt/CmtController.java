ea75a3abb12c9d41e668b5d32307428d62a89603
JeongHoBBang <dlawjdghbn@naver.com>
2023-05-31 17:05:38 +0900

    password change

--- src/main/java/com/green/boardver3/cmt/CmtController.java
+++ src/main/java/com/green/boardver3/cmt/CmtController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/board")
 public class CmtController {
     private final CmtService service;
+
     @Autowired
     public CmtController(CmtService service) {
         this.service = service;
