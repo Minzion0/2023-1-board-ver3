474f31f7e9f20f95235d3d998d2a7193e4bd1d64
JeongHoBBang <dlawjdghbn@naver.com>
2023-06-02 16:53:36 +0900

    a

--- src/main/java/com/green/boardver3/utils/FileUtils.java
+++ src/main/java/com/green/boardver3/utils/FileUtils.java
@@ -1,10 +1,15 @@
 package com.green.boardver3.utils;
 
 
+import java.util.UUID;
+
 public class FileUtils {
     public static void main(String[] args) {
-        String ext = getFileNm("임정호.하하.hi");
+        String ext = getFileNm("임정호.하하.jpg");
         System.out.println("ext = " + ext);
+
+        String sdf = makeRandomFileNm("gkgkgk.jpg");
+        System.out.println("sdf = " + sdf);
     }
 
 //    public static String getExt(String fileNm){
@@ -14,5 +19,11 @@ public class FileUtils {
     public static String getFileNm(String fileNm){
 
         return fileNm.substring(0,fileNm.lastIndexOf("."));
+    }
+    public static String makeRandomFileNm(String fileNm){
+        String uuID = UUID.randomUUID().toString();
+        String random = fileNm.substring(fileNm.lastIndexOf("."));
+        String urd = uuID+random;
+        return urd;
     }
 }
