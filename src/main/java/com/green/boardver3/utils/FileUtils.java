79687e5e484897489cf21ccf4e69ab69445a8191
minzion0 <aert12435@gmail.com>
2023-06-02 16:53:22 +0900

    cmtdt

--- src/main/java/com/green/boardver3/utils/FileUtils.java
+++ src/main/java/com/green/boardver3/utils/FileUtils.java
@@ -1,11 +1,24 @@
 package com.green.boardver3.utils;
 
 
+import java.util.UUID;
+
 public class FileUtils {
     public static void main(String[] args) {
         String ext = getFileNm("임정호.하하.hi");
         System.out.println("ext = " + ext);
+
+        String s = makeRandomFileNm("하하호호호.jpg");
+        System.out.println("s = " + s);
     }
+
+    public static String makeRandomFileNm(String fileNm){
+        String uuid = UUID.randomUUID().toString();
+        String substring = fileNm.substring(fileNm.lastIndexOf("."));
+        String RdNm = uuid + substring;
+        return RdNm;
+    }
+
 
 //    public static String getExt(String fileNm){
 //        return fileNm.substring(fileNm.lastIndexOf(".")+1);
